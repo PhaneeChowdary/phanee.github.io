2abf6ec080980efbfcbd2addd141eeb7da8f2d6d
PhaneeChowdary <phaneendrababugunturu4393@gmail.com>
2025-03-27 03:29:47 -0400

    Portfolio

--- next.config.js
+++ next.config.js
@@ -4,8 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/Portfolio' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '',
 }
 
 module.exports = nextConfig
